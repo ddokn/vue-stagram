538a8b73042e9c6fcbb0f0852d2914b1a6a6ca9f
김도근 <ddokn@tdi9.com>
2023-01-31 17:35:00 +0900

    [0131] part3 1~3

---
+++ src/components/Container.vue
@@ -0,0 +1,25 @@
+<template>
+	<div>
+		<Post 
+		v-for="(a, i) in postData"
+		:key="i"
+		:postData="postData[i]"/>
+	</div>
+</template>
+
+<script>
+import Post from './Post.vue'
+export default {
+	name: 'ContainerVue',
+	components: {
+		Post
+	},
+	props: {
+		postData: Array
+	}
+}
+</script>
+
+<style>
+
+</style>
